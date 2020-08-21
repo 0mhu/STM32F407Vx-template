bdcfb2c29c5a51ef06bd968ad7c9146100332e58
Mario Hüttel <mario.huettel@gmx.net>
2020-08-21 23:16:55 +0200

    Add full vector table and implement region tests in main. Verified by debugger

--- main.c
+++ main.c
@@ -1,6 +1,20 @@
 #include <stm32/stm32f4xx.h>
 
-void main(void)
+int main(void)
 {
+	int stack_init = 124;
+	int stack_var;
+	static volatile int data_var = 0x5557;
+	static volatile int bss_var;
+	static volatile int __attribute__((section(".ccm.data"))) ccm_data_var = 0x1224;
+	static volatile int __attribute__((section(".ccm.bss"))) ccm_bss_var;
+
+	stack_var = 100;
+	bss_var = 200;
+	ccm_bss_var = 300;
+	ccm_bss_var = stack_init + stack_var + bss_var + data_var + ccm_data_var + ccm_bss_var;
+
+	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
+	
 	while (1);
 }
--- startup/startup_stm32f407vx.c
+++ startup/startup_stm32f407vx.c
@@ -45,16 +45,100 @@ WEAK void __int_default_handler(void);
 void Reset_Handler(void);
 void NMI_Handler(void) ALIAS(__int_default_handler);
 void HardFault_Handler(void) ALIAS(__int_default_handler);
-void SVCall_Handler(void) ALIAS(__int_default_handler);
+void MemManage_Handler(void) ALIAS(__int_default_handler);
+void BusFault_Handler(void) ALIAS(__int_default_handler);
+void UsageFault_Handler(void) ALIAS(__int_default_handler);
+void SVC_Handler(void) ALIAS(__int_default_handler);
+void DebugMon_Handler(void) ALIAS(__int_default_handler);
 void PendSV_Handler(void) ALIAS(__int_default_handler);
 void SysTick_Handler(void) ALIAS(__int_default_handler);
 
 /* Peripheral Interrupts (by default mapped onto Default Handler) */
-
-    
+void WWDG_IRQHandler(void) ALIAS(__int_default_handler);
+void PVD_IRQHandler(void) ALIAS(__int_default_handler);
+void TAMP_STAMP_IRQHandler(void) ALIAS(__int_default_handler);
+void RTC_WKUP_IRQHandler(void) ALIAS(__int_default_handler);
+void FLASH_IRQHandler(void) ALIAS(__int_default_handler);
+void RCC_IRQHandler(void) ALIAS(__int_default_handler);
+void EXTI0_IRQHandler(void) ALIAS(__int_default_handler);
+void EXTI1_IRQHandler(void) ALIAS(__int_default_handler);
+void EXTI2_IRQHandler(void) ALIAS(__int_default_handler);
+void EXTI3_IRQHandler(void) ALIAS(__int_default_handler);
+void EXTI4_IRQHandler(void) ALIAS(__int_default_handler);
+void DMA1_Stream0_IRQHandler(void) ALIAS(__int_default_handler);
+void DMA1_Stream1_IRQHandler(void) ALIAS(__int_default_handler);
+void DMA1_Stream2_IRQHandler(void) ALIAS(__int_default_handler);
+void DMA1_Stream3_IRQHandler(void) ALIAS(__int_default_handler);
+void DMA1_Stream4_IRQHandler(void) ALIAS(__int_default_handler);
+void DMA1_Stream5_IRQHandler(void) ALIAS(__int_default_handler);
+void DMA1_Stream6_IRQHandler(void) ALIAS(__int_default_handler);
+void ADC_IRQHandler(void) ALIAS(__int_default_handler);
+void CAN1_TX_IRQHandler(void) ALIAS(__int_default_handler);
+void CAN1_RX0_IRQHandler(void) ALIAS(__int_default_handler);
+void CAN1_RX1_IRQHandler(void) ALIAS(__int_default_handler);
+void CAN1_SCE_IRQHandler(void) ALIAS(__int_default_handler);
+void EXTI9_5_IRQHandler(void) ALIAS(__int_default_handler);
+void TIM1_BRK_TIM9_IRQHandler(void) ALIAS(__int_default_handler);
+void TIM1_UP_TIM10_IRQHandler(void) ALIAS(__int_default_handler);
+void TIM1_TRG_COM_TIM11_IRQHandler(void) ALIAS(__int_default_handler);
+void TIM1_CC_IRQHandler(void) ALIAS(__int_default_handler);
+void TIM2_IRQHandler(void) ALIAS(__int_default_handler);
+void TIM3_IRQHandler(void) ALIAS(__int_default_handler);
+void TIM4_IRQHandler(void) ALIAS(__int_default_handler);
+void I2C1_EV_IRQHandler(void) ALIAS(__int_default_handler);
+void I2C1_ER_IRQHandler(void) ALIAS(__int_default_handler);
+void I2C2_EV_IRQHandler(void) ALIAS(__int_default_handler);
+void I2C2_ER_IRQHandler(void) ALIAS(__int_default_handler);
+void SPI1_IRQHandler(void) ALIAS(__int_default_handler);
+void SPI2_IRQHandler(void) ALIAS(__int_default_handler);
+void USART1_IRQHandler(void) ALIAS(__int_default_handler);
+void USART2_IRQHandler(void) ALIAS(__int_default_handler);
+void USART3_IRQHandler(void) ALIAS(__int_default_handler);
+void EXTI15_10_IRQHandler(void) ALIAS(__int_default_handler);
+void RTC_Alarm_IRQHandler(void) ALIAS(__int_default_handler);
+void OTG_FS_WKUP_IRQHandler(void) ALIAS(__int_default_handler);
+void TIM8_BRK_TIM12_IRQHandler(void) ALIAS(__int_default_handler);
+void TIM8_UP_TIM13_IRQHandler(void) ALIAS(__int_default_handler);
+void TIM8_TRG_COM_TIM14_IRQHandler(void) ALIAS(__int_default_handler);
+void TIM8_CC_IRQHandler(void) ALIAS(__int_default_handler);
+void DMA1_Stream7_IRQHandler(void) ALIAS(__int_default_handler);
+void FSMC_IRQHandler(void) ALIAS(__int_default_handler);
+void SDIO_IRQHandler(void) ALIAS(__int_default_handler);
+void TIM5_IRQHandler(void) ALIAS(__int_default_handler);
+void SPI3_IRQHandler(void) ALIAS(__int_default_handler);
+void UART4_IRQHandler(void) ALIAS(__int_default_handler);
+void UART5_IRQHandler(void) ALIAS(__int_default_handler);
+void TIM6_DAC_IRQHandler(void) ALIAS(__int_default_handler);
+void TIM7_IRQHandler(void) ALIAS(__int_default_handler);
+void DMA2_Stream0_IRQHandler(void) ALIAS(__int_default_handler);
+void DMA2_Stream1_IRQHandler(void) ALIAS(__int_default_handler);
+void DMA2_Stream2_IRQHandler(void) ALIAS(__int_default_handler);
+void DMA2_Stream3_IRQHandler(void) ALIAS(__int_default_handler);
+void DMA2_Stream4_IRQHandler(void) ALIAS(__int_default_handler);
+void ETH_IRQHandler(void) ALIAS(__int_default_handler);
+void ETH_WKUP_IRQHandler(void) ALIAS(__int_default_handler);
+void CAN2_TX_IRQHandler(void) ALIAS(__int_default_handler);
+void CAN2_RX0_IRQHandler(void) ALIAS(__int_default_handler);
+void CAN2_RX1_IRQHandler(void) ALIAS(__int_default_handler);
+void CAN2_SCE_IRQHandler(void) ALIAS(__int_default_handler);
+void OTG_FS_IRQHandler(void) ALIAS(__int_default_handler);
+void DMA2_Stream5_IRQHandler(void) ALIAS(__int_default_handler);
+void DMA2_Stream6_IRQHandler(void) ALIAS(__int_default_handler);
+void DMA2_Stream7_IRQHandler(void) ALIAS(__int_default_handler);
+void USART6_IRQHandler(void) ALIAS(__int_default_handler);
+void I2C3_EV_IRQHandler(void) ALIAS(__int_default_handler);
+void I2C3_ER_IRQHandler(void) ALIAS(__int_default_handler);
+void OTG_HS_EP1_OUT_IRQHandler(void) ALIAS(__int_default_handler);
+void OTG_HS_EP1_IN_IRQHandler(void) ALIAS(__int_default_handler);
+void OTG_HS_WKUP_IRQHandler(void) ALIAS(__int_default_handler);
+void OTG_HS_IRQHandler(void) ALIAS(__int_default_handler);
+void DCMI_IRQHandler(void) ALIAS(__int_default_handler);
+void CRYP_IRQHandler(void) ALIAS(__int_default_handler);
+void HASH_RNG_IRQHandler(void) ALIAS(__int_default_handler);
+void FPU_IRQHandler(void) ALIAS(__int_default_handler);    
 
 extern int main(void);
-extern void __system_init(void);
+extern void SystemInit(void);
 
 extern void __ld_top_of_stack(void);
 #if defined(_cplusplus)
@@ -67,6 +151,101 @@ void (* const vector_table[])(void) SECTION(".vectors") = {
 	Reset_Handler,
 	NMI_Handler,
 	HardFault_Handler,
+	MemManage_Handler,
+	BusFault_Handler,
+	UsageFault_Handler,
+	0,
+	0,
+	0,
+	0,
+	SVC_Handler,
+	DebugMon_Handler,
+	0,
+	PendSV_Handler,
+	SysTick_Handler,
+	/* Peripheral Interrupts */
+	WWDG_IRQHandler,
+	PVD_IRQHandler,
+	TAMP_STAMP_IRQHandler,
+	RTC_WKUP_IRQHandler,
+	FLASH_IRQHandler,
+	RCC_IRQHandler,
+	EXTI0_IRQHandler,
+	EXTI1_IRQHandler,
+	EXTI2_IRQHandler,
+	EXTI3_IRQHandler,
+	EXTI4_IRQHandler,
+	DMA1_Stream0_IRQHandler,
+	DMA1_Stream1_IRQHandler,
+	DMA1_Stream2_IRQHandler,
+	DMA1_Stream3_IRQHandler,
+	DMA1_Stream4_IRQHandler,
+	DMA1_Stream5_IRQHandler,
+	DMA1_Stream6_IRQHandler,
+	ADC_IRQHandler,
+	CAN1_TX_IRQHandler,
+	CAN1_RX0_IRQHandler,
+	CAN1_RX1_IRQHandler,
+	CAN1_SCE_IRQHandler,
+	EXTI9_5_IRQHandler,
+	TIM1_BRK_TIM9_IRQHandler,
+	TIM1_UP_TIM10_IRQHandler,
+	TIM1_TRG_COM_TIM11_IRQHandler,
+	TIM1_CC_IRQHandler,
+	TIM2_IRQHandler,
+	TIM3_IRQHandler,
+	TIM4_IRQHandler,
+	I2C1_EV_IRQHandler,
+	I2C1_ER_IRQHandler,
+	I2C2_EV_IRQHandler,
+	I2C2_ER_IRQHandler,
+	SPI1_IRQHandler,
+	SPI2_IRQHandler,
+	USART1_IRQHandler,
+	USART2_IRQHandler,
+	USART3_IRQHandler,
+	EXTI15_10_IRQHandler,
+	RTC_Alarm_IRQHandler,
+	OTG_FS_WKUP_IRQHandler,
+	TIM8_BRK_TIM12_IRQHandler,
+	TIM8_UP_TIM13_IRQHandler,
+	TIM8_TRG_COM_TIM14_IRQHandler,
+	TIM8_CC_IRQHandler,
+	DMA1_Stream7_IRQHandler,
+	FSMC_IRQHandler,
+	SDIO_IRQHandler,
+	TIM5_IRQHandler,
+	SPI3_IRQHandler,
+	UART4_IRQHandler,
+	UART5_IRQHandler,
+	TIM6_DAC_IRQHandler,
+	TIM7_IRQHandler,
+	DMA2_Stream0_IRQHandler,
+	DMA2_Stream1_IRQHandler,
+	DMA2_Stream2_IRQHandler,
+	DMA2_Stream3_IRQHandler,
+	DMA2_Stream4_IRQHandler,
+	ETH_IRQHandler,
+	ETH_WKUP_IRQHandler,
+	CAN2_TX_IRQHandler,
+	CAN2_RX0_IRQHandler,
+	CAN2_RX1_IRQHandler,
+	CAN2_SCE_IRQHandler,
+	OTG_FS_IRQHandler,
+	DMA2_Stream5_IRQHandler,
+	DMA2_Stream6_IRQHandler,
+	DMA2_Stream7_IRQHandler,
+	USART6_IRQHandler,
+	I2C3_EV_IRQHandler,
+	I2C3_ER_IRQHandler,
+	OTG_HS_EP1_OUT_IRQHandler,
+	OTG_HS_EP1_IN_IRQHandler,
+	OTG_HS_WKUP_IRQHandler,
+	OTG_HS_IRQHandler,
+	DCMI_IRQHandler,
+	CRYP_IRQHandler,
+	HASH_RNG_IRQHandler,
+	FPU_IRQHandler
 };
 
 static void __init_section(unsigned int *src_start, unsigned int *dest_start, unsigned int *dest_end) {
@@ -89,6 +268,9 @@ static void __fill_zero(unsigned int *start, unsigned int *end) {
 extern unsigned int __ld_load_data;
 extern unsigned int __ld_sdata_ccm;
 extern unsigned int __ld_edata_ccm;
+extern unsigned int __ld_load_ccm_data;
+extern unsigned int __ld_sdata_ccm;
+extern unsigned int __ld_edata_ccm;
 extern unsigned int __ld_sbss_ccm;
 extern unsigned int __ld_ebss_ccm;
 extern unsigned int __ld_sdata;
@@ -99,7 +281,7 @@ extern unsigned int __ld_sheap;
 extern unsigned int __ld_eheap;
 
 void Reset_Handler(void) {
-	/* Stack is already initilized by hardware */
+	/* Stack is already initialized by hardware */
 
 	/* Copy .data section */
 	__init_section(&__ld_load_data, &__ld_sdata, &__ld_edata);
@@ -107,9 +289,10 @@ void Reset_Handler(void) {
 	__fill_zero(&__ld_sbss, &__ld_ebss);
 	/* Fill Heap with zero */
 	__fill_zero(&__ld_sheap, &__ld_eheap);
-
 	/* Fill static CCM memory with zeroes */
 	__fill_zero(&__ld_sbss_ccm, &__ld_ebss_ccm);
+	/* Init CCM RAM data section */
+	__init_section(&__ld_load_ccm_data, &__ld_sdata_ccm, &__ld_edata_ccm);
 
 	/* Set clocks, waitstates, ART operation etc. */
 	SystemInit();
